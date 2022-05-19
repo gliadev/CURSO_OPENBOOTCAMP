d7be95507b3d936e8e2398d3571ab85c97a302ef
gliatem <gliatem@gmail.com>
2022-05-19 15:48:48 +0200

    subo practica de persona

---
+++ Java Basico/javaBasico/src/com/conceptos/Text.java
@@ -0,0 +1,10 @@
+package com.conceptos;
+
+import javax.swing.*;
+
+public class Text {
+    public static void main(String[] args) {
+         int entero = JOptionPane.showConfirmDialog("Introduce un entero");
+
+    }
+}
