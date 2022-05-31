274ae4c7a6d18188d19ede1384ffca245f6aefe4
gliatem <gliatem@gmail.com>
2022-05-31 19:10:32 +0200

    volviendo hacer la estructura tras el fallo

--- Java Basico/sesiones_4_5_6/ob-poo/src/poo/coninterfaces/EmpleadoReader.java
+++
@@ -1,4 +0,0 @@
-package poo.coninterfaces;
-
-public interface EmpleadoReader {
-}
--- Java Basico/sesiones_4_5_6/ob-poo/src/poo/herencia/Coche.java
+++
@@ -1,8 +0,0 @@
-package poo.herencia;
-
-import poo.clases.Vehiculo;
-
-public class Coche extends Vehiculo {
-
-    int numPuertas;
-}
