0c204da94bfbacec7c75f0fd93f68673815335d7
gliatem <gliatem@gmail.com>
2022-06-12 20:05:53 +0200

    ejercicio html css y algo de angular

--- angular/HolaMundo/src/app/components/saludo/saludo.component.ts
+++ angular/HolaMundo/src/app/components/saludo/saludo.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy,Input } from '@angular/core';
 
 @Component({
   selector: 'app-saludo',
   templateUrl: './saludo.component.html',
   styleUrls: ['./saludo.component.scss']
 })
-export class SaludoComponent implements OnInit {
+export class SaludoComponent implements OnInit, OnDestroy {
 
-  nombre: string = "Adolfo";
+  @Input() nombre: string = "Anónimo";
 
   constructor() { }
-
+  
   ngOnInit(): void {
     // instrucciones previas al a renderizaion del componente
-
+    
     console.log("ngOnInit del componente")
   }
+  
+  /*
+  Ejemplo para gestionar un evento de tipo click en el DOM
+  
+  */
+ 
+ alertaSaludo(): void{  
+   alert(`Hola, ${this.nombre}.Alerta depacha desde un clic de boton`)
+  }
+  
+  ngOnDestroy(): void {
+    console.log("Componente suludo desaparece");
+  }
+  
 
 }
---
+++ angular/ProyectoFinal/src/app/components/task/task.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ITask } from 'src/app/models/interfaces/Task.interface';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.scss']
+})
+export class TaskComponent implements OnInit {
+
+  @Input() task: ITask | undefined;
+  @Output() delete: EventEmitter<ITask> = new EventEmitter<ITask>();
+  
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  deleteTask(){
+    
+    this.delete.emit(this.task);
+  }
+
+}
