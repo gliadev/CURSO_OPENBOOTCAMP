88fe0c3e463c82b104c2ab6b059283fe22404998
gliatem <gliatem@gmail.com>
2022-06-27 22:00:43 +0200

    ejercicio 12

--- JAVASCRIPT Basico/sesion-11-programacion-orientada-a-objetos/instanciacion-clases.js
+++ JAVASCRIPT Basico/sesion-11-programacion-orientada-a-objetos/instanciacion-clases.js
@@ -1,24 +1,26 @@
 class Persona {
-    constructor(nombre, edad, isDeveloper) {
-        this.nombre = nombre
-        this.edad = edad
-        this.isDeveloper = isDeveloper
-    }
+  constructor(nombre, edad, isDeveloper) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.isDeveloper = isDeveloper;
+  }
 
-    saludo() {
-        console.log(`Hola, mi nombre es ${this.nombre}, tengo ${this.edad} años.`)
-    }
+  saludo() {
+    console.log(`Hola, mi nombre es ${this.nombre}, tengo ${this.edad} años.`);
+  }
 }
 
-const nueva_persona = new Persona("Gorka", 34, true)
-console.log(nueva_persona)
-nueva_persona.saludo()
+const nueva_persona = new Persona("Gorka", 34, true);
+console.log(nueva_persona);
+nueva_persona.saludo();
 
-let numero = 60 // inicializar
-console.log(typeof numero)
+const nueva_persona1 = new Persona("Adolfo", 43, false);
 
-let persona_2 = new Persona("Maria", 34, false) // instanciar
-console.log(typeof persona_2)
-console.log(persona_2 instanceof Persona)
+let numero = 60; // inicializar
+console.log(typeof numero);
+
+let persona_2 = new Persona("Maria", 34, false); // instanciar
+console.log(typeof persona_2);
+console.log(persona_2 instanceof Persona);
 
 // instanceof -> similar a typeof pero para clases
