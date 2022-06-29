5b228be84da6613e8275e0e0336d6520951c032a
gliatem <gliatem@gmail.com>
2022-06-29 22:40:22 +0200

    subida de varios

---
+++ JAVASCRIPT Basico/Tareas/ejercicio14/index.js
@@ -0,0 +1,7 @@
+let boton = document.querySelector("button");
+
+boton.addEventListener("click", () => alert("LE DISTE CLISK"));
+
+$("button").click(function () {
+  console.log("usando jQuery");
+});
