08d211e14196a4048e59ff0eebd38a657b22a9ce
gliatem <gliatem@gmail.com>
2022-07-06 15:08:22 +0200

    varios de C y alguno de java

---
+++ C#/tareas/Tarea02/ejercicio03/Program.cs
@@ -0,0 +1,13 @@
+﻿int num1 = 58;
+int num2 = 123;
+
+if (num1 > num2)
+{
+    Console.WriteLine("El numero 1 es mayor que el numero 2");
+} else if(num1 < num2)
+{
+    Console.WriteLine("El numero 2 es mayor que el numro 1");
+} else if (num2 == num1)
+{
+    Console.WriteLine("Los numero son iguales");
+}
---
+++ C#/tareas/Tarea02/ejercicio2/Program.cs
@@ -0,0 +1,14 @@
+﻿string vehiculo = "Coche";
+int puertas = 4;
+int ruedas = 4;
+string marca = "Toyota";
+Boolean ITV = true;
+
+
+string mueble = "Mesa";
+double peso = 15.0d;
+double largo = 78.0d;
+string material = "Madera";
+string color = "Rojo";
+
+Console.WriteLine(vehiculo);
