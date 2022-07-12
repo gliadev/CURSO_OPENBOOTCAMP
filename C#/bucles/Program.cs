addad72ac21c90a1a817f63bc2a60d915b792290
gliatem <gliatem@gmail.com>
2022-07-13 00:47:28 +0200

    otro mas

---
+++ C#/bucles/Program.cs
@@ -0,0 +1,11 @@
+﻿// Bucles 
+
+// WHILE - MIENTRAS
+
+int i = 0;
+while (i <= 10)
+{
+    // codigo que se ejecuta mientras la condiciones es true
+    Console.WriteLine("Este es un bucle while" + i);
+    i++;
+}
---
+++ C#/tareas/Tarea04/ejercicio01/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Ejercicio 1 - While
+
+Escribe una tabla de multiplicar del 1 al 10 para un número entero que recibe por consola. Es decir, un programa que presente para el 1:
+
+1 x 1 = 1
+1 x 2 = 2
+…
+1 x 10 = 10
+*/
+
+Console.WriteLine("Introduce un numero ");
+int numero = Int32.Parse(Console.ReadLine());
+int contador = 0;
+
+
+while(contador <= 10)
+{
+    Console.WriteLine(numero + " x " + contador + " = " + (numero*contador));
+    contador++;
+}
